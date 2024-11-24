dd84074ff1b6e6868fcc72ffa81c033fa2157ed3
Zayats Natalia <n.zayats74@gmail.com>
2024-11-24 23:06:12 +0400

    Update UserRepository, UserService and UserServiceImpl

--- src/main/java/com/beautyhub/Controllers/UserController.java
+++ src/main/java/com/beautyhub/Controllers/UserController.java
@@ -1,6 +1,7 @@
 package com.beautyhub.Controllers;
 
-
+import com.beautyhub.Entity.User;
+import com.beautyhub.Service.UserService;
 import com.beautyhub.DTO.UserResponseDTO;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
@@ -8,14 +9,30 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/clients")
+@RequestMapping("/users")
 @AllArgsConstructor
 @Validated
 public class UserController {
+    private UserService service;
+
     @PostMapping("/create")
     public ResponseEntity<String> createClient(@Valid @RequestBody UserResponseDTO userDTO) {
         // Если валидация пройдет, выполнится этот код
         return ResponseEntity.ok("Клиент успешно создан");
     }
+
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "Welcome";
+    }
+
+    @GetMapping("/all-users")
+    public List<User> allUsers(){
+        return service.findAllUsers();
+    }
+
+
 }
