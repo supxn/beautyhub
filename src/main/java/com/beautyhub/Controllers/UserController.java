171c9ef383d4c748b095379d93a0244ec98d488c
Zayats Natalia <n.zayats74@gmail.com>
2024-12-04 22:16:20 +0400

    update: Update UserControllers

--- src/main/java/com/beautyhub/Controllers/UserController.java
+++ src/main/java/com/beautyhub/Controllers/UserController.java
@@ -1,5 +1,6 @@
 package com.beautyhub.Controllers;
 
+import com.beautyhub.DTO.UserRequestDTO;
 import com.beautyhub.Entity.User;
 import com.beautyhub.Service.UserService;
 import com.beautyhub.DTO.UserResponseDTO;
@@ -8,16 +9,22 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import io.swagger.v3.oas.annotations.Operation;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
 @AllArgsConstructor
 @Validated
 public class UserController {
+    private final UserService userService;
     private UserService service;
 
+    @Operation(
+            summary = "Создать нового пользователя",
+            description = "Создает нового пользователя.")
     @PostMapping("/create")
     public ResponseEntity<String> createClient(@Valid @RequestBody UserResponseDTO userDTO) {
         // Если валидация пройдет, выполнится этот код
@@ -29,10 +36,35 @@ public class UserController {
         return "Welcome";
     }
 
+    @Operation(
+            summary = "Получить всех пользователей",
+            description = "Возвращает список всех пользователей.")
     @GetMapping("/all-users")
     public List<User> allUsers(){
-        return service.findAllUsers();
+        return service.getList();
     }
 
+    @Operation(
+            summary = "Получить пользователя по ID",
+            description = "Возвращает пользователя с указанным ID.")
+    @GetMapping("/{id}")
+    public UserResponseDTO getUser(@PathVariable Long id) {
+        return service.GetById(id);
+    }
 
+    @Operation(
+            summary = "Удалить пользователя",
+            description = "Удаляет пользователя с указанным ID.")
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+    }
+
+    @Operation(
+            summary = "Обновить данные пользователя",
+            description = "Обновляет данные пользователя с указанным ID.")
+    @PatchMapping("/{id}")
+    public UserResponseDTO updateUser(@PathVariable Long id, @RequestBody UserRequestDTO userDTO) {
+        return userService.update(id, userDTO);
+    }
 }
--- src/main/java/com/beautyhub/Service/UserService.java
+++ src/main/java/com/beautyhub/Service/UserService.java
@@ -1,18 +1,22 @@
 package com.beautyhub.Service;
 
+import com.beautyhub.DTO.UserRequestDTO;
+import com.beautyhub.DTO.UserResponseDTO;
 import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Service;
 import com.beautyhub.Entity.User;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.IntStream;
 
 
 public interface UserService{
-    List<User> findAllUsers();
+    List<User> getList();
     //List<User> allUsers();
     User saveUser(User user);
     User findByEmail(String email);
-    User updateUser(User user);
-    void deleteUser(String email);
-    User userById(long id);
+    void deleteUser(Long id);
+    UserResponseDTO GetById(Long id);
+    UserResponseDTO update(Long id, UserRequestDTO userRequestDTO);
+
 }
